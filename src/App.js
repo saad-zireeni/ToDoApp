27fa7820fe02b9f4daf54c717c7355636c800991
amrokh1996 <amrrrkh1996@gmail.com>
2023-05-22 12:06:27 +0300

    upload react project

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signup from './components/Signup';
+import Login from './components/login';
+import About from './components/about';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signUp" element={<Signup />} />
+        <Route path="/aboutUs" element={<About />} />
+        <Route path="/contactUs" element={<Contact />} />
+      </Routes>
+      <Footer />
+  </BrowserRouter> 
+  );
+}
+
+export default App;
